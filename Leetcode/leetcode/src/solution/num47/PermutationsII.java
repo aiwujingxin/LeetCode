0c3f6d07526035665a4b7733ed9d3962c4b8d56f
aiwujingxin <aiwujingxin@gmail.com>
2018-10-06 23:19:20 +0800

    git

--- Leetcode/leetcode/src/solution/num47/PermutationsII.java
+++ Leetcode/leetcode/src/solution/num47/PermutationsII.java
@@ -1,5 +1,7 @@
 package solution.num47;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,7 +13,73 @@ import java.util.List;
 
 public class PermutationsII {
 
+
+    /**
+     *
+     * time: O(n! *n) , space :O(n)
+     * */
     public List<List<Integer>> permuteUnique(int[] nums) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
+        helper(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> list, int[] nums, boolean[] used) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] || i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
+            list.add(nums[i]);
+            helper(res, list, nums, used);
+            used[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
+
+    /**
+     * time :O(n!) space:O(n)
+     *
+     * */
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
+        helper2(res, nums, 0);
+        return res;
+    }
+
+    private void helper2(List<List<Integer>> res, int[] nums, int start) {
+        if (start == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int num : nums) {
+                list.add(num);
+            }
+            res.add(list);
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            if (isUsed(nums, start, i)) continue;
+            swap(nums, start, i);
+            helper2(res, nums,start + 1 );
+            swap(nums, start ,i);
+        }
+    }
+
+    private void swap(int[] nums, int l, int r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+    }
+
+    private boolean isUsed(int[] nums, int i, int j) {
+        for (int k = i; k < j; k++) {
+            if (nums[k] == nums[j]) return true;
+        }
+        return false;
     }
 }
