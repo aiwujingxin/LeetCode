c1a987db7c8980f8e27bfbcd267af11192f48261
aiwujingxin <aiwujingxin@gmail.com>
2018-10-06 19:22:01 +0800

    git

--- LeetCode/algorithms/java/src/solution/num41/FirstMissingPositive.java
+++ LeetCode/algorithms/java/src/solution/num41/FirstMissingPositive.java
@@ -9,9 +9,30 @@ package solution.num41;
 
 public class FirstMissingPositive {
 
+    /**
+     *
+     * nums[nums[i] - 1] = nums[i];
+     *
+     * time O(n)
+     * space O(1)
+     * */
+
     public int firstMissingPositive(int[] nums) {
 
+        if (nums == null || nums.length == 0 ) return 1;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] < nums.length && nums[nums[i] - 1]!= nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
 
-        return 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i +1 ;
+            }
+        }
+        return nums.length + 1;
     }
 }
