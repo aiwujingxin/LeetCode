54aeeb910f9292d8057e110618d7967daec9cc78
aiwujingxin <aiwujingxin@gmail.com>
2018-10-06 15:34:07 +0800

    git

--- LeetCode/algorithms/java/src/solution/num23/MergekSortedLists.java
+++ LeetCode/algorithms/java/src/solution/num23/MergekSortedLists.java
@@ -2,18 +2,63 @@ package solution.num23;
 
 import baseclass.ListNode;
 
+import java.util.PriorityQueue;
+
 /**
  * @program: LeetCode
  * @description:
  * @author: jingxin Wu
  * @create: 2018-10-06 15:06
  **/
-
+/**
+ *
+ * time: O(nlogk) k = number of lists
+ * space : O(n)
+ * */
 public class MergekSortedLists {
     public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0 ) return null;
+        return sort(lists, 0 ,lists.length -1 );
+    }
 
+    private ListNode sort(ListNode[] lists, int lo, int hi) {
+        if (lo >= hi) return lists[lo];
+        int mid = (hi - lo) /2 + lo;
 
-        return null;
+        ListNode l1 = sort(lists, lo , mid);
+        ListNode l2 = sort(lists, mid + 1, hi);
 
+        return merge(l1, l2);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        }
+        l2.next = merge(l1 , l2.next);
+        return l2;
+    }
+
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists == null || lists.length == 0 ) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length , (a, b) ->a.val - b.val);
+        ListNode dummy  = new ListNode(0);
+        ListNode cur = dummy;
+        for (ListNode list : lists) {
+            if (list != null) {
+                queue.add(list);
+            }
+        }
+        while (!queue.isEmpty()) {
+            cur.next = queue.poll();
+            cur = cur.next;
+            if (cur.next != null) {
+                queue.add(cur.next);
+            }
+        }
+        return dummy.next;
     }
 }
---
+++ LeetCode/algorithms/java/src/solution/num41/FirstMissingPositive.java
@@ -0,0 +1,17 @@
+package solution.num41;
+
+/**
+ * @program: LeetCode
+ * @description: 缺失的第一个正数
+ * @author: jingxin Wu
+ * @create: 2018-10-06 15:32
+ **/
+
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+
+
+        return 0;
+    }
+}
