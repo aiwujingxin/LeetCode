31ae771770b5e26c38137e3f2b4d60c754c3dfbd
aiwujingxin <aiwujingxin@gmail.com>
2018-10-07 21:38:44 +0800

    git

--- Leetcode/leetcode/src/solution/num1_100/num01/TowSum.java
+++ Leetcode/leetcode/src/solution/num1_100/num01/TowSum.java
@@ -1,11 +1,28 @@
 package solution.num1_100.num01;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: LeetCode
+ * @description:
+ * @author: jingxin Wu
+ * @create: 2018-10-07 21:37
+ **/
+
 public class TowSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
 
-    public int[] twoSum(int[] nums, int target) {
-
-
-
-        return null;
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i ;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i );
+        }
+        return result;
     }
 }
